eba91cea99f85f673fb03c14f4335c6fc0bf7a2f
Muhammad Arief <mgi.official1@gmail.com>
2024-09-01 16:26:20 +0700

    chore: refactor and beautify

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ def main():
     text = get_book_text(book_path)
     words = splitWords(text)
     countChars = countChar(text)
-    wordTuple = list(countChars.items())
-    print(f"--- Bein report of books/frankenstein.txt ---\n")
-    #print(f"{countChars} words found in the document\n\n")
+    print("--- Bein report of books/frankenstein.txt ---\n")
+    # print(f"{countChars} words found in the document\n\n")
     words = sorted(countChars.items(), key=lambda x: x[1], reverse=True)
     for word, count in words:
         print(f"The word '{word}' was found {count} times")
 
     print("--- End report ---")
 
+
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
 
+
 def splitWords(file_contents):
     words = len(file_contents.split())
     return words
+
 
 def countChar(words):
     chars = {}
@@ -31,7 +33,9 @@ def countChar(words):
                 chars[char] = 1
     return chars
 
+
 def sort_on(dict):
     return dict["num"]
+
 
 main()
